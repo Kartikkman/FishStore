bc46fc08e4bcb5958dfe67962293bbfa9e7c36be
Kartik Sikka <sikkakartik07@gmail.com>
2021-07-04 00:57:02 +0530

    Adding CSS to React App

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
 import StorePicker from './components/StorePicker';
+import "./css/style.css";
 
 render(<StorePicker />, document.querySelector('#main'));
+
+/*
+Primarly , There are two ways to add css inside the React App 
+1.Import the CSS inside the Index.html 
+2.Importing CSS respective to the Component inside the JS file for that component 
+
+
+As of now , we will be importing CSS inside the index.js file which will be applied to complete react Project . 
+*/
