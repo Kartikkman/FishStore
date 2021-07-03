945aa4a28c76c76e5ab30899cda90b9e98b50e84
Kartik Sikka <sikkakartik07@gmail.com>
2021-07-03 23:59:19 +0530

    Learn Component Building

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StorePicker extends React.Component{
+
+    render(){
+        //return <p>I am the store Picker!</p>
+        return React.createElement('p',{ className: 'hey'}, 'I am the store Picker!')
+    }
+
+
+}
+
+export default StorePicker;
